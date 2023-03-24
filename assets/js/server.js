2447acff1138736e28b173e2025c7f8c86df0dd9
Billy_Joe <williamjcollier4@gmail.com>
2023-03-24 15:14:16 +0800

    cloudflare

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,1 +1,19 @@
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// // const port = 3000
 
+// const SECRET_KEY = '0x4AAAAAAADZNPgnBONeWhNlqxXUuvn3Js8'
+
+
+// app.use(express.urlencoded({ extended: false}))
+// app.use(express.json())
+// const listener = app.listen(process.env.PORT || 3000, () => {
+//   console.log("Listening on port " + listener.address().port)
+// })
+
+// app.get("/", (req, res) => {
+//   res.sendFile(_dir + "../index.html")
+// })
+
+// app.post("/verify")
